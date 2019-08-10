ba152c353390af603ee15783ecf5744d5d037e5f
Jan Scheithauer <j.scheithauer@gmail.com>
2019-08-11 01:42:03 +0200

    Add recieving sensor data with delegate

--- Sources/FloraKit.swift
+++ Sources/FloraKit.swift
@@ -13,11 +13,21 @@ public class FloraKit: NSObject {
     private let floraService = FloraService()
 
     public func start() {
+        floraService.delegate = self
         floraService.scan(duration: 5) {[weak self] peripherals in
             for peripheral in peripherals {
                 self?.floraService.read(peripheral: peripheral)
             }
             print("Whoop: \(peripherals.count)")
+            /*_ = Timer.scheduledTimer(withTimeInterval: 10.0, repeats: true) { [weak self] timer in
+             self?.floraService.read(peripheral: peripheral)
+             }*/
         }
+    }
+}
+
+extension FloraKit: FloraServiceDelegate {
+    func floraService(_ service: FloraService, didRecieveSensorData sensorData: FloraService.SensorData) {
+        print(sensorData.debugDescription)
     }
 }
--- Sources/FloraService.swift
+++ Sources/FloraService.swift
@@ -6,19 +6,54 @@
 import Foundation
 import CoreBluetooth
 
+protocol FloraServiceDelegate {
+    func floraService(_ service: FloraService, didRecieveSensorData sensorData: FloraService.SensorData)
+}
+
 class FloraService: NSObject {
+    
+    public class SensorData: CustomDebugStringConvertible {
+        let sensorId: UUID
+        fileprivate(set) var temp: Float
+        fileprivate(set) var lux: Int
+        fileprivate(set) var moisture: Int
+        fileprivate(set) var conductivity: Int
+        fileprivate(set) var battery: Int
+
+        var debugDescription: String {
+            return "sensorId: \(sensorId.uuidString),\ntemp: \(temp),\nlux: \(lux),\nmoisture: \(moisture),\nconductivity: \(conductivity),\nbattery: \(battery)"
+        }
+        
+        init(sensorId: UUID) {
+            self.sensorId = sensorId
+            self.temp = 0
+            self.lux = 0
+            self.moisture = 0
+            self.conductivity = 0
+            self.battery = 0
+        }
+
+    }
 
     private static let miFloraPrefix = "FE95"
     private var discoveredSensors: [CBPeripheral] = []
+    private var discoveredSensorData: [UUID: SensorData] = [:]
+    open var delegate: FloraServiceDelegate?
     private lazy var manager = CBCentralManager(delegate: self, queue: DispatchQueue.main, options: nil)
 
     private let serviceUUID = CBUUID(string: "00001204-0000-1000-8000-00805F9B34FB")
     private let writeModeUUID = CBUUID(string: "00001A00-0000-1000-8000-00805F9B34FB")
     private let sensorDataUUID = CBUUID(string: "00001A01-0000-1000-8000-00805F9B34FB")
     private let batteryUUID = CBUUID(string: "00001A02-0000-1000-8000-00805F9B34FB")
+    
+    public convenience init(delegate: FloraServiceDelegate?) {
+        self.init()
+        self.delegate = delegate
+    }
 
     func scan(duration: Int, completion: @escaping (_ floraDevices: [CBPeripheral]) -> Void) {
         _ = manager
+        self.discoveredSensors = []
         DispatchQueue.main.asyncAfter(deadline: .now() + Double(duration)) { [weak self] in
             guard let self = self else { return }
             self.manager.stopScan()
@@ -26,15 +61,12 @@ class FloraService: NSObject {
         }
     }
 
-    func discoverPeripherals() {
+    private func discoverPeripherals() {
         manager.scanForPeripherals(withServices: [CBUUID(string: FloraService.miFloraPrefix)], options: nil)
     }
 
     func read(peripheral: CBPeripheral) {
         manager.connect(peripheral)
-        /*_ = Timer.scheduledTimer(withTimeInterval: 10.0, repeats: true) { [weak self] timer in
-            self?.manager.connect(peripheral)
-        }*/
     }
 }
 
@@ -53,15 +85,16 @@ extension FloraService: CBCentralManagerDelegate {
     }
 
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-        print("✅", "Connected to:", "\(peripheral.name ?? "")")
+        print("✅", "Connected to:", "\(peripheral.name ?? ""), \(peripheral.identifier.uuidString)")
+        discoveredSensorData[peripheral.identifier] = SensorData(sensorId: peripheral.identifier)
         peripheral.delegate = self
         peripheral.discoverServices(nil)
     }
 }
 
 extension FloraService: CBPeripheralDelegate {
+
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
-        //print(peripheral.services?.map({ ($0, $0.uuid.uuidString) }))
         guard let writeService = peripheral.services?.filter({ $0.uuid == serviceUUID }).first else {
             return
         }
@@ -77,7 +110,7 @@ extension FloraService: CBPeripheralDelegate {
     }
 
     func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
-        guard let dataAndBatteryCharacteristics = characteristic.service.characteristics?.filter( {$0.uuid == sensorDataUUID || $0.uuid == batteryUUID}) else {
+        guard let dataAndBatteryCharacteristics = characteristic.service.characteristics?.filter({ $0.uuid == sensorDataUUID || $0.uuid == batteryUUID }) else {
             return
         }
         let dataCharacteristic = dataAndBatteryCharacteristics[0]
@@ -94,16 +127,35 @@ extension FloraService: CBPeripheralDelegate {
             assertionFailure("Did magical data write happend before?")
             return
         }
-
+    
+        guard let sensorData = discoveredSensorData[peripheral.identifier] else {
+            assertionFailure("No SensorData in didUpdateValueFor!")
+            return
+        }
+        
         if characteristic.uuid == sensorDataUUID {
-            let rawTemp0 =  Float(UInt16(value[0])) / 10.0
-            let lux =  Int((value[3] + value[4]))
+            let rawTemp0 = Float(UInt16(value[0])) / 10.0
+            let lux = Int((value[3] + value[4]))
             let moisture = Int(value[7])
             let conductivity = Int((value[8] + value[9]))
-            print("temp: \(rawTemp0),\nlux: \(lux),\nmoisture: \(moisture),\nconductivity: \(conductivity)")
+            
+            sensorData.temp = rawTemp0
+            sensorData.lux = lux
+            sensorData.moisture = moisture
+            sensorData.conductivity = conductivity
         } else if characteristic.uuid == batteryUUID {
-            print("battery: \(Int(value[0]))")
+            sensorData.battery = Int(value[0])
             manager.cancelPeripheralConnection(peripheral)
         }
+        //TODO: manager.cancelPeripheralConnection(peripheral) only when all characteristics were seen, e.g. if we change order of peripheral.readValue(for: ...) then cancel gets called to early
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        guard let sensorData = discoveredSensorData[peripheral.identifier] else {
+            assertionFailure("SensorData was removed before!")
+            return
+        }
+        discoveredSensorData[peripheral.identifier] = nil
+        delegate?.floraService(self, didRecieveSensorData: sensorData)
     }
 }
